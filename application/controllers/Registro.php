fe8bda612fa5cbc8c6ae89eca9bcff11625872a4
Juan Chavez <jchavez666@gmail.com>
2021-06-15 17:51:56 +0300

    good

--- application/controllers/Registro.php
+++ application/controllers/Registro.php
@@ -3,7 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Registro extends CI_Controller {
 
+    public function __construct() {
+		parent::__construct();
+		$this->load->helper(array('getmenu'));
+
+        // !cargamos nuestra base de datos
+        $this->load->database();
+	}
+
     public function index() {
-        $this->load->view('registro');
+        $data['menu'] = main_menu();
+        $this->load->view('registro', $data);
+
+        // !hacemos la query hacia la tabla usuarios
+        $query = $this->db->get('usuarios');
+        var_dump($query->result());
     }
+
+    public function create(){
+        $username = $this->input->post('username');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $password_c = $this->input->post('password_confirm');
+
+        var_dump($username . $email . $password . $password_c);
+    }
+
+
 }
--- application/views/registro.php
+++ application/views/registro.php
@@ -8,5 +8,35 @@
     </head>
     <body>
         <h1>Registro</h1>
+
+        <ul>
+            <?php foreach($menu as $item): ?>
+                <li><a href="<?= $item['url'] ?>"><?=$item['title'] ?></a></li>
+            <?php endforeach; ?>
+
+        </ul>
+
+        <?php 
+
+        // !VAMOS A ENVIAR LOS DATOS A EL CONTROLADOR REGISTRO/ FUNCION CREATE
+            echo form_open('registro/create', array('method' => 'POST'));
+            echo form_label('Nombre de usuario:');
+            echo form_input('username');
+
+            echo "<br>";
+            echo form_label('Correo Electronico');
+            echo form_input(array('type' => 'email', 'name' =>'email'));
+
+            echo "<br>";
+            echo form_label('Contrasena');
+            echo form_password('password');
+
+            echo "<br>";
+            echo form_label('Confirmacion de contrasena');
+            echo form_password('password_confirm');
+            
+            echo form_submit('submit', 'Enviar Datos');
+            echo form_close();
+        ?>
     </body>
 </html>
